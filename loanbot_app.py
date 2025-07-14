17c29e8a2f2efbfea197530628880f4d51af2d6e
Tanya Shrivastava <140186118+tanyashrivastava21@users.noreply.github.com>
2025-07-14 11:28:57 +0530

    loanbot_app.py

--- loanbot_app.py
+++ loanbot_app.py
@@ -17,7 +17,7 @@ import google.generativeai as genai
 
 # Load Gemini API Key from secrets
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("models/gemini-pro")
+model = genai.GenerativeModel(model_name = "models/gemini-pro")
 
 # Load and clean the dataset
 @st.cache_resource
